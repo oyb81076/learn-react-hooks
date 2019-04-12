c60b21e919cc202245bdce9004a9c237eef7b315
欧阳斌 <ouyangbin81076@gmail.com>
2019-04-12 15:25:23 +0800

    增加 useMapDispatch 的功能, 使之和 react-redux 中的 connect 一致

--- packages/redux-hooks/src/counter/CounterHandler.tsx
+++ packages/redux-hooks/src/counter/CounterHandler.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { RenderDisplay } from '../components/RenderDisplay';
-import { useMapAction, useMapDispatch } from '../hooks';
+import { useMapDispatch } from '../hooks';
 import { addAction, decrAction, incrAction } from './counterReducer';
 
 function mapCounterAction(dispatch: any) {
@@ -8,20 +8,25 @@ function mapCounterAction(dispatch: any) {
     add(value: number) { dispatch(addAction(value)) },
   }
 }
+function mapAdd200(dispatch: any){
+  return ()=> dispatch(addAction(200));
+}
 const mapAction = {
   decr: decrAction,
   incr: incrAction,
 }
 export function CounterHandler() {
-  const actions = useMapDispatch(mapCounterAction)
-  const { incr, decr } = useMapAction(mapAction);
-  const add100 = React.useCallback(() => actions.add(100), [actions.add])
+  const { add } = useMapDispatch(mapCounterAction)
+  const { incr, decr } = useMapDispatch(mapAction);
+  const add100 = React.useCallback(() => add(100), [add])
+  const add200 = useMapDispatch(mapAdd200);
   return (
     <div style={{ marginBottom: 20 }}>
       <RenderDisplay name="CounterHandler" />
       <button onClick={incr}>incr</button>
       <button onClick={decr}>decr</button>
       <button onClick={add100}>add 100 by thunk action</button>
+      <button onClick={add200}>add 200 by useMapDispatch((dispatch)=> ()=> dispatch(add(200)))</button>
     </div>
   );
 }
--- packages/redux-hooks/src/hooks.ts
+++ packages/redux-hooks/src/hooks.ts
@@ -32,15 +32,17 @@ export function useMapState<T, S = any>(
 }
 
 function strictEquals<T>(a: T, b: T) { return a === b; }
-
-export function useMapDispatch<T, A extends Action = AnyAction>(mapDispatch: ((dispatch: Dispatch<A>) => T)) {
+export function useMapDispatch<T, A extends Action = AnyAction>(mapDispatch: ((dispatch: Dispatch<A>) => T)): T;
+export function useMapDispatch<T extends ActionCreatorsMapObject>(mapAction: T): T;
+export function useMapDispatch(mapDispatch: any) {
   const store = useContext(ReduxStoreContext);
-  return useMemo(() => mapDispatch(store.dispatch), [store.dispatch, mapDispatch]);
-}
-
-export function useMapAction<T extends ActionCreatorsMapObject>(mapAction: T): T {
-  const store = useContext(ReduxStoreContext);
-  return useMemo(() => bindActionCreators(mapAction, store.dispatch), [mapAction]);
+  return useMemo(() => {
+    if (typeof mapDispatch === "function") {
+      return mapDispatch(store.dispatch)
+    } else {
+      return bindActionCreators(mapDispatch, store.dispatch);
+    }
+  }, [store.dispatch, mapDispatch]);
 }
 
 export function useDispatch() {
